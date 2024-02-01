554b47353daf27a468a2871fcccdc5a4cd05cf70
shakibul22 <shakibulislam684@gmail.com>
2024-02-02 00:00:23 +0600

    redesign

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import About from "../Components/About";
 import Banner from "../Components/Banner";
+import NewsLetter from "../Components/NewsLetter";
+import OurPartners from "../Components/OurPartners";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <About/>
+            <NewsLetter/>
+            <OurPartners/>
             
         </div>
     );
